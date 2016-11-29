e5322a407cd60a4802ff59bee7b459e3c7076c6e
Dewayne Allen <dewayneallen76@gmail.com>
2016-11-29 15:30:42 -0600

    ANGULAR LESSONS FROM CODE SCHOOL

---
+++ public/js/gemStore.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+	var app = angular.module('gemStore', []);
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem = { name: 'Azurite', price: 2.95 };
+	
+})();
