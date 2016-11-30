50121c5bcd4b03d14340e869ad65e785ff5d7bad
Dewayne Allen <dewayneallen76@gmail.com>
2016-11-30 14:12:43 -0600

    ADDED IMAGE FOR GEM, WORKED ON ADDING TABS FOLLOWING CODE SCHOOL VIDEOS

--- public/js/gemStore.js
+++ public/js/gemStore.js
@@ -3,9 +3,48 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = { name: 'Azurite', price: 2.95 };
-	
+	app.controller('TabController', function(){
+		this.tab = 1;
+
+		this.setTab = function(newValue) {
+			this.tab = newValue;
+		};
+
+		this.isSet = function(tabName) {
+			return this.tab === tabName;
+		};
+	});
+
+	var gems = [
+		{ 
+			name: 'Azurite',
+			description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.", 
+			shine: 8,
+			price: 110.50,
+			images: [
+				"img/gem.jpg"
+			]
+		},
+		{ 	name: 'Bloodstone', 
+			description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+			shine: 9,
+			price: 22.90,
+			images: [
+				"img/gem.jpg"
+			]
+		},
+		{
+			name: 'Zircon',
+			description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+			shine: 70,
+			price: 1100, 
+			images: [
+				"img/gem.jpg"
+			]
+		}
+	];
+
 })();
